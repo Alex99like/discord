6d095a4a485b82e20c53904087222b5b2cdca4b4
Alexander <aleksievisa@gmail.com>
2023-09-02 02:32:31 +0300

    feat: add crea server modal

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,9 +2,12 @@
 
 import { Plus } from "lucide-react"
 import { ActionTooltip } from "../action-tooltip"
+import { useModal } from "@/hooks/use-modal-store"
 
 export const NavigationAction = () => {
-return (
+  const { onOpen } = useModal()
+
+  return (
     <div>
       <ActionTooltip
         side="right"
@@ -12,7 +15,7 @@ return (
         label="Add a server"
       >
         <button
-          //onClick={() => onOpen("createServer")}
+          onClick={() => onOpen("createServer")}
           className="group flex items-center"
         >
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
