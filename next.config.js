e248fd50d4b7e7e28b3bbb6d13f7c9a8a8b0bd6d
Aleksander <76603765+Alex99like@users.noreply.github.com>
2023-11-24 19:49:51 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      "uploadthing.com"
-    ]
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
