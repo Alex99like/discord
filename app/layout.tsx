25d21219fff73fb31453573b80139cf1d9c479ee
Aleksander <76603765+Alex99like@users.noreply.github.com>
2023-11-26 00:37:00 +0300

    feat: add messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -24,17 +25,19 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn(
           font.className,
-          'bg-white dark:bg-[#313338]'
+          "bg-white dark:bg-[#313338]"
         )}>
           <ThemeProvider
-            attribute='class'
-            defaultTheme='dark'
-            enableSystem
-            storageKey='discord-theme'
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
